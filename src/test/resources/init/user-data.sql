677f2f23d7a3d0ff9e3cdb15195b7b2c20461fbd
panik5467 <53363577+panik5467@users.noreply.github.com>
2023-05-01 20:39:41 +0200

    Add files via upload

---
+++ src/test/resources/init/user-data.sql
@@ -0,0 +1,6 @@
+-- INSERT 5 DATA FIXTURES
+INSERT INTO T_USER (name, email) VALUES ('Roger Clara', 'alicia.marty@gmail.com');
+INSERT INTO T_USER (name, email) VALUES ('Camille Guérin', 'rayan.sanchez@yahoo.fr');
+INSERT INTO T_USER (name, email) VALUES ('Julien Mael', 'laura.royer@yahoo.fr');
+INSERT INTO T_USER (name, email) VALUES ('Gérard Mael', 'victor.dupuis@hotmail.fr');
+INSERT INTO T_USER (name, email) VALUES ('Dubois Anaïs', 'alice.lemoine@hotmail.fr');
